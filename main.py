6a1c9b779fe46923ffafa3ad70a625516532a510
Saulo Leão <sleaojr@gmail.com>
2021-03-08 23:38:21 -0300

    :recycle: added a single comma

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ async def predict(texto: Texto):
     return get_prediction(texto.corpo)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0" port="8080")
+    uvicorn.run("main:app", host="0.0.0.0", port="8080")
