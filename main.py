0391ea19446a5d56dc93b3537cc30ca8a59baa8a
Saulo Leão <sleaojr@gmail.com>
2021-03-08 23:26:16 -0300

    :rocket: running programmatically

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from model.model import get_prediction
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import uvicorn
 
 class Texto(BaseModel):
     corpo: str
@@ -25,3 +26,6 @@ app.add_middleware(
 @app.post("/")
 async def predict(texto: Texto):
     return get_prediction(texto.corpo)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0")
